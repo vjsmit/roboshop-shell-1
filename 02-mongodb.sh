d19e0ff81135c9f37cfd147967fa60cb4f602767
smit <vj.smit97@gmail.com>
2023-06-01 11:09:32 -0400

    Installing mongodb via script

--- 02-mongodb.sh
+++ 02-mongodb.sh
@@ -1,8 +1,11 @@
-#Setup the MongoDB repo file
+echo -e "\e[32mCopy mongodb repo file\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-yum install mongodb-org -y
+echo -e "\e[32mInstall mongodb\e[0m"
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 
-systemctl enable mongod
-systemctl restart mongod
+echo -e "\e[32mstart the service\e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log

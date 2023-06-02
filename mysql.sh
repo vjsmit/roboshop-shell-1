68f5a6c1705280de1e928e824ea8a76a4db8667d
smit <vj.smit97@gmail.com>
2023-06-02 04:41:41 -0400

    setup mysql componenet

--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,16 @@
-echo "\e[33m disable MySQL 8\e[0m"
+echo "\e[33mdisable MySQL 8\e[0m"
 yum module disable mysql -y &>>/tmp/roboshop.log
 
-echo "\e[33m Setup the MySQL5.7 repo file\e[0m"
+echo "\e[33mSetup the MySQL5.7 repo file\e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 
-echo "\e[33m Install MySQL Server\e[0m"
+echo "\e[33mInstall MySQL Server\e[0m"
 yum install mysql-community-server -y
 
-echo "\e[33m Start MySQL Service\e[0m"
+echo "\e[33mStart MySQL Service\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
 systemctl restart mysqld &>>/tmp/roboshop.log
 
-echo "\e[33m Change the default root password \e[0m"
+echo "\e[33mChange the default root password \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 

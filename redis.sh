fd4e468a84b11529f3d188ffa722896818cc8b77
smit <vj.smit97@gmail.com>
2023-06-02 04:00:39 -0400

    setup redis listen address bug

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ echo -e "\e[33mInstall Redis\e[0m"
 yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdate redis listen address\e[0m"
-sed -i 's|127.0.0.1|0.0.0.0| /etc/redis.conf' &>>/tmp/roboshop.log
+sed -i 's|127.0.0.1|0.0.0.0|' /etc/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33mStart the redis service\e[0m"
 systemctl enable redis &>>/tmp/roboshop.log

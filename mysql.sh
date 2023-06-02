03fc475a2599ffdcb931abb812fbbbd19be94b2b
smit <vj.smit97@gmail.com>
2023-06-02 04:45:28 -0400

    setup mysql componenet

--- mysql.sh
+++ mysql.sh
@@ -2,10 +2,11 @@ echo -e "\e[33mdisable MySQL 8\e[0m"
 yum module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup the MySQL5.7 repo file\e[0m"
-yum install mysql-community-server -y &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell-1/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall MySQL Server\e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
 
 echo -e "\e[33mStart MySQL Service\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log

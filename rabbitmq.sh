dbc3582a09ab88b04cc64b1dc067b00b352efd29
smit <vj.smit97@gmail.com>
2023-06-02 08:31:47 -0400

    setup rabbitmq componenet

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,7 @@
 echo -e "\e[33mConfigure YUM Repos for erlang \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m Configure YUM Repos for rabbitmq\e[0m"
+echo -e "\e[33mConfigure YUM Repos for rabbitmq\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall RabbitMQ \e[0m"
@@ -11,6 +11,6 @@ echo -e "\e[33mStart RabbitMQ Service \e[0m"
 systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl restart rabbitmq-server &>>/tmp/roboshop.log
 
-echo -e "\e[33m Create one user for the app and set permission\e[0m"
+echo -e "\e[33mCreate one user for the app and set permission\e[0m"
 rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log

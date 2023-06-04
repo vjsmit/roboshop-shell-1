5bf5457ecd78e9ffa69a3dacd15a5f3454f11e7a
vjsmit <vj.smit97@gmail.com>
2023-06-04 13:20:15 +0530

    trying set -e option

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,18 +1,18 @@
 source common.sh
 
 func_print_head "Configure YUM Repos for erlang"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
 
 func_print_head "Configure YUM Repos for rabbitmq\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
 
 func_print_head "Install RabbitMQ"
-yum install rabbitmq-server -y &>>/tmp/roboshop.log
+yum install rabbitmq-server -y &>>${log_file}
 
 func_print_head "Start RabbitMQ Service"
-systemctl enable rabbitmq-server &>>/tmp/roboshop.log
-systemctl restart rabbitmq-server &>>/tmp/roboshop.log
+systemctl enable rabbitmq-server &>>${log_file}
+systemctl restart rabbitmq-server &>>${log_file}
 
 func_print_head "Create one user for the app and set permission\e[0m"
-rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log &>>/tmp/roboshop.log
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+rabbitmqctl add_user roboshop roboshop123 &>>${log_file} &>>${log_file}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}

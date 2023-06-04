29a8c254b2e1e2d83147e91d7c8ff974bedf6da7
vjsmit <vj.smit97@gmail.com>
2023-06-04 15:38:54 +0530

    giving input to variable

--- common.sh
+++ common.sh
@@ -6,6 +6,14 @@ func_print_head() {
   echo -e "\e[32m$1\e[0m"
   echo -e "\e[31m$1\e[0m" &>>${log_file}
 }
+stat_check() {
+  if [ $1 -eq 0 ]; then
+     echo SUCCESS
+  else
+     echo FAILURE
+   fi
+
+}
 
 app_prereq() {
     func_print_head "Add application User"
@@ -13,86 +21,49 @@ app_prereq() {
     if [ $? -ne 0 ]; then
         useradd roboshop &>>${log_file}
     fi
-    
-    if [ $? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+    stat_check $?
 
     func_print_head "Setup an app directory"
     rm -rf ${app_path}
     mkdir ${app_path} &>>${log_file}
-    if [ $? -eq 0 ]; then
-        echo SUCCESS
-    else
-        echo FAILURE
-    fi
+    stat_check $?
 
     func_print_head "Download the application code"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
     cd ${app_path}
     unzip /tmp/${component}.zip &>>${log_file}
-     if [ $? -eq 0 ]; then
-        echo SUCCESS
-    else
-        echo FAILURE
-     fi
+    stat_check $?
 }
 
 service_start(){
     func_print_head "Load the service"
     systemctl daemon-reload &>>${log_file}
-    if [ $? -eq 0 ]; then
-            echo SUCCESS
-    else
-            echo FAILURE
-    fi
+    stat_check $?
 
     func_print_head "Start the service"
     systemctl enable ${component} &>>${log_file}
     systemctl restart ${component} &>>${log_file}
-    if [ $? -eq 0 ]; then
-            echo SUCCESS
-    else
-            echo FAILURE
-    fi
+    stat_check $?
 }
 
 func_nodejs() {
     func_print_head "Setup NodeJS repo"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-    if [ $? -eq 0 ]; then
-        echo SUCCESS
-    else
-         echo FAILURE
-    fi
+    stat_check $?
 
     func_print_head "Install NodeJS"
     yum install nodejs -y &>>${log_file}
-    if [ $? -eq 0 ]; then
-       echo SUCCESS
-    else
-       echo FAILURE
-    fi
+    stat_check $?
 
     app_prereq
 
     func_print_head "Download the dependencies"
     npm install &>>${log_file}
-    if [ $? -eq 0 ]; then
-            echo SUCCESS
-    else
-            echo FAILURE
-    fi
+    stat_check $?
 
     func_print_head "Setup SystemD Catalogue Service"
     cp /home/centos/roboshop-shell-1/${component}.service /etc/systemd/system/${component}.service
-    if [ $? -eq 0 ]; then
-            echo SUCCESS
-    else
-            echo FAILURE
-    fi
+    stat_check $?
 
     service_start
 }

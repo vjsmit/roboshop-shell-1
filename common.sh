dc29f016148d238e93d4d481ab9d1a1923d830ea
vjsmit <vj.smit97@gmail.com>
2023-06-04 13:59:32 +0530

    Creating app path variable

--- common.sh
+++ common.sh
@@ -1,23 +1,27 @@
 log_file="/tmp/roboshop.log"
+app_path="/app"
 rm -rf ${log_file}
 
 func_print_head() {
-  echo -e "\e[33m$1\e[0m"
+  echo -e "\e[32m$1\e[0m"
   echo -e "\e[31m$1\e[0m" &>>${log_file}
 }
 
 app_prereq() {
     func_print_head "Add application User"
     useradd roboshop &>>${log_file}
+    echo $?
 
     func_print_head "Setup an app directory"
-    rm -rf /app
-    mkdir /app &>>${log_file}
+    rm -rf ${app_path}
+    mkdir ${app_path} &>>${log_file}
+    echo $?
 
     func_print_head "Download the application code"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-    cd /app
+    cd ${app_path}
     unzip /tmp/${component}.zip &>>${log_file}
+    echo $?
 }
 
 service_start(){
@@ -28,20 +32,25 @@ service_start(){
     systemctl enable ${component} &>>${log_file}
     systemctl restart ${component} &>>${log_file}
 }
+
 func_nodejs() {
     func_print_head "Setup NodeJS repo"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+    echo $?
 
     func_print_head "Install NodeJS"
     yum install nodejs -y &>>${log_file}
+    echo $?
 
     app_prereq
 
     func_print_head "Download the dependencies"
     npm install &>>${log_file}
+    echo $?
 
     func_print_head "Setup SystemD Catalogue Service"
     cp /home/centos/roboshop-shell-1/${component}.service /etc/systemd/system/${component}.service
+    echo $?
 
     service_start
 }
@@ -54,7 +63,7 @@ mongo_schema_setup() {
   yum install mongodb-org-shell -y &>>${log_file}
 
   func_print_head "Load Schema"
-  mongo --host mongodb-dev.smitdevops.online </app/schema/${component}.js &>>${log_file}
+  mongo --host mongodb-dev.smitdevops.online <${app_path}/schema/${component}.js &>>${log_file}
 }
 
 mysql_schema_setup() {
@@ -62,7 +71,7 @@ mysql_schema_setup() {
     yum install mysql -y &>>${log_file}
 
     func_print_head "Load Schema"
-    mysql -h mysql-dev.smitdevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log_file}
+    mysql -h mysql-dev.smitdevops.online -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql &>>${log_file}
 
     func_print_head "Restart ${component} service"
     systemctl restart ${component} &>>${log_file}
